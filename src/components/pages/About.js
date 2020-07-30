a18f53064ba3bf05803c39d26f3a9523fd626786
rajaneehs <rrajaneesh5@gmail.com>
2020-07-30 17:22:35 +0530

    feat: layout components & basic pages

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,8 +3,8 @@ import React, { Fragment } from "react";
 const About = () => {
 	return (
 		<Fragment>
-			<h1>About this React application</h1>
 			<p>Application to search GitHub Users</p>
+			<p>React, Hooks, ContextAPI</p>
 			<p>Version: 1.0.0</p>
 		</Fragment>
 	);
